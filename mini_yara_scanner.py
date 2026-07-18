77929117e79fbd28ecd79f134617fa43fc641274
Somsouk Thepphavong <patrickaus2023@gmail.com>
2026-07-18 14:52:33 +1000

    minor changes and added to the code

--- mini_yara_scanner.py
+++ mini_yara_scanner.py
@@ -5,18 +5,21 @@ rules = [
     {
         "name": "Mimikatz Command",
         "pattern": "sekurlsa::logonpasswords",
+        "severity": "high",
     },
     {
         "name": "PowerShell Download",
         "pattern": "Invoke-WebRequest",
+        "severity": "medium",
     },
     {
         "name": "Encoded PowerShell",
         "pattern": "-EncodedCommand",
+        "severity": "high",
     },
 ]
 
-testfile_path = Path(__file__).parent / "test_files"/ "normal.txt"
+testfile_path = Path(__file__).parent / "test_files"/ "powershell.txt"
 
 
 def read_text_file(file_path):
@@ -37,5 +40,21 @@ def read_text_file(file_path):
         return None
 
 
-test = read_text_file(testfile_path)
-print(test)
+def match_rules(content, rules):
+    matches = []
+    for rule in rules:
+        if rule["pattern"].lower() in content:
+            match = {
+                "rule_name": rule["name"],
+                "pattern": rule["pattern"],
+                "severity": rule["severity"],
+            }
+            matches.append(match)
+    return matches
+
+content = read_text_file(testfile_path)
+content = content.lower()
+
+if content is not None:
+    matches = match_rules(content, rules)
+    print(matches)
